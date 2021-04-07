6d222d2ff1383efa620fe141a2dc275e5a07784c
Edinaldo Soares <ednaldosdl@gmail.com>
2021-04-07 19:41:25 -0300

    :package: função para movimentação dentro da DIV

--- js/script.js
+++ js/script.js
@@ -8,8 +8,10 @@ function start() {
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
 	// Principais variáveis do jogo
-	var jogo = {}
-	var TECLA = {
+	let posicaoY = parseInt(Math.random() * 334);
+	let velocidade = 5;
+	let jogo = {}
+	let TECLA = {
 		W: 87,
 		S: 83,
 		D: 68
@@ -31,6 +33,9 @@ function start() {
 	function loop() {
 		movefundo();
 		movejogador();
+		moveinimigo1();
+		moveinimigo2();
+		moveamigo();
 	}
 
 	//Função que movimenta o fundo do jogo
@@ -41,17 +46,55 @@ function start() {
 
 	function movejogador() {
 		if (jogo.pressionou[TECLA.W]) {
-			var topo = parseInt($("#jogador").css("top"));
+			let topo = parseInt($("#jogador").css("top"));
 			$("#jogador").css("top",topo-10);
+
+			if (topo<=0) {
+				$("#jogador").css("top",topo+10);
+			}
 		}
 		
 		if (jogo.pressionou[TECLA.S]) {
-			var topo = parseInt($("#jogador").css("top"));
-			$("#jogador").css("top",topo+10);	
+			let topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo+10);
+
+			if (topo>=434) {	
+				$("#jogador").css("top",topo-10);
+			}	
 		}
 		
 		if (jogo.pressionou[TECLA.D]) {	
 		//Chama função Disparo	
+		}
+	}
+
+	function moveinimigo1() {
+		posicaoX = parseInt($("#inimigo1").css("left"));
+		$("#inimigo1").css("left",posicaoX-velocidade);
+		$("#inimigo1").css("top",posicaoY);
+			
+		if (posicaoX<=0) {
+			posicaoY = parseInt(Math.random() * 334);
+			$("#inimigo1").css("left",694);
+			$("#inimigo1").css("top",posicaoY);	
+		}
+	}
+
+	function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+		$("#inimigo2").css("left",posicaoX-3);
+				
+		if (posicaoX<=0) {
+		$("#inimigo2").css("left",775);		
+		}
+	}
+
+	function moveamigo() {
+		posicaoX = parseInt($("#amigo").css("left"));
+		$("#amigo").css("left",posicaoX+1);
+					
+		if (posicaoX>906) {
+		$("#amigo").css("left",0);		
 		}
 	}
 }
